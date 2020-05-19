cef727e0e5bd1e209ed77e4a51fc059ff455b917
Atalya Alon <atalya.alon@gmail.com>
2020-05-19 03:45:54 +0300

    add partial student teacher flow

--- notebooks/teacher.py
+++ notebooks/teacher.py
@@ -2,14 +2,20 @@ import os
 import torch
 import openpifpaf
 from pathlib import Path
+from pycocotools.coco import COCO
+from openpifpaf.datasets import CocoKeypoints
+from openpifpaf import transforms as openpifpaf_transforms
+from openpifpaf.eval_coco import EvalCoco
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-BASE_DIR = Path('../')
+BASE_DIR = Path('../../')
 DATASETS_PATH = BASE_DIR.joinpath('datasets')
 DATASET_NAME = 'data-mscoco'
 COCO_DATASET_PATH = DATASETS_PATH.joinpath(DATASET_NAME)
 COCO_VAL_PATH = COCO_DATASET_PATH.joinpath('images/val2017')
+annFile = COCO_DATASET_PATH.joinpath('annotations/person_keypoints_val2017.json')
+
 
 class Teacher:
     def __init__(self,
@@ -20,8 +26,6 @@ class Teacher:
                  checkpoint=None):
         self.teacher=teacher
         self.output = output if output else 'temp_output'
-        if not output:
-            os.mkdir(self.output)
         self.data_percentage=data_percentage
         self.unlabel_data_dir=unlabel_data_dir
         self.checkpoint=checkpoint
@@ -34,30 +38,49 @@ class Teacher:
             self.net = net_cpu.to(device)
         else:
             # train
-            os.system("python -m openpifpaf.train \
-                      --lr=1e-3 \
-                      --momentum=0.95 \
-                      --epochs=150 \
-                      --lr-decay 120 140 \
-                      --batch-size=16 \
-                      --basenet=resnet101 \
-                      --head-quad=1 \
-                      --headnets pif paf paf25 \
-                      --square-edge=401 \
-                      --lambdas 10 1 1 15 1 1 15 1 1 \
-                      --output {}".format(self.output))
-            net_cpu, _ = openpifpaf.network.factory(checkpoint=self.output)
+        ############################################################################################
+        # We need to train the model and eval using our OKS - in order to know when to stop
+        # perhaps we can use some kind of loop - similar to what is suggested in openpifpaf:
+        ############################################################################################
+
+############################################################################################
+#         while true; do \
+#   CUDA_VISIBLE_DEVICES=0 find outputs/ -name "resnet101block5-pif-paf-l1-190109-113346.pkl.epoch???" -exec \
+#     python3 -m openpifpaf.eval_coco --checkpoint {} -n 500 --long-edge=641 --skip-existing \; \
+#   ; \
+#   sleep 300; \
+# done
+############################################################################################
+
+            # os.system("python -m openpifpaf.train \
+            #           --lr=1e-3 \
+            #           --momentum=0.95 \
+            #           --epochs=150 \
+            #           --lr-decay 120 140 \
+            #           --batch-size=16 \
+            #           --basenet=resnet101 \
+            #           --head-quad=1 \
+            #           --headnets pif paf paf25 \
+            #           --square-edge=401 \
+            #           --lambdas 10 1 1 15 1 1 15 1 1 \
+            #           --output {}".format(self.output))
+            net_cpu, _ = openpifpaf.network.factory(checkpoint=self.checkpoint)
             self.net = net_cpu.to(device)
 
     def teach(self):
         # create pseudo labels
 
-        openpifpaf.decoder.CifSeeds.threshold = 0.5
-        openpifpaf.decoder.nms.Keypoints.keypoint_threshold = 0.2
-        openpifpaf.decoder.nms.Keypoints.instance_threshold = 0.2
-        processor = openpifpaf.decoder.factory_decode(self.net.head_nets,
-                                                          basenet_stride=self.net.base_net.stride)
-        from openpifpaf.datasets import CocoKeypoints
+        decode = openpifpaf.decoder.factory_decode(self.net,
+                                                   seed_threshold=0.5)
+        processor = openpifpaf.decoder.Processor(self.net, decode,
+                                                 instance_threshold=0.2,
+                                                 keypoint_threshold=0.3)
+
+        preprocess = openpifpaf_transforms.Compose([
+                    openpifpaf_transforms.NormalizeAnnotations(),
+                    openpifpaf_transforms.RescaleAbsolute(641),
+                    openpifpaf_transforms.EVAL_TRANSFORM,
+                ])
         coco_person_val_ds = CocoKeypoints(root=COCO_VAL_PATH,
                                            annFile=annFile,
                                            preprocess=preprocess,
@@ -76,18 +99,21 @@ class Teacher:
 
             images_batch = images_batch.to(device)
             fields_batch = processor.fields(images_batch)
-
-            annIds = coco_kps.getAnnIds(catIds=catIds)
-            anns = coco_kps.loadAnns(annIds)
             predictions = processor.annotations(fields=fields_batch[0])
+
+        ############################################################################################
+        # We need to save annotations and add path path to teacher, and also choose only relevant annotations
+        ############################################################################################
 
             # initialize COCO api for person keypoints annotations
             coco_kps=COCO(annFile)
             ec = EvalCoco(coco_kps, processor, preprocess.annotations_inverse)
 
-            ec.from_predictions(predictions,
-                                    meta_batch[0],
-                                    debug=False,
-                                    gt=ann_ids,
-                                    image_cpu=images_batch)
+            from_pred_results = ec.from_predictions(predictions,
+                                                    meta_batch[0],
+                                                    debug=False,
+                                                    gt=ann_ids,
+                                                    image_cpu=images_batch)
+            print(from_pred_results)
+            break
 
