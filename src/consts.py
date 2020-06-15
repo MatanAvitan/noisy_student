9bb2aa739e06028937319ae6f21751800bea6404
Atalya Alon <atalya.alon@gmail.com>
2020-06-15 18:38:37 +0300

    add cd openpifpaf to commands

--- src/consts.py
+++ src/consts.py
@@ -12,39 +12,42 @@ TRAIN_IMAGE_DIR = os.getenv('TRAIN_IMAGE_DIR')
 OUTPUT_DIR = os.getenv('OUTPUT_DIR')
 EVAL_DIR = os.getenv('OUTPUT_DIR')
 
-TRAIN_COMMAND = """python -m openpifpaf.train \
-                      --lr=0.05 \
-                      --momentum=0.9 \
-                      --epochs={num_train_epochs} \
-                      --lr-warm-up-epochs=1 \
-                      --lr-decay 220 \
-                      --lr-decay-epochs=30 \
-                      --lr-decay-factor=0.01 \
-                      --batch-size=32 \
-                      --square-edge=385 \
-                      --lambdas 1 1 0.2   1 1 1 0.2 0.2    1 1 1 0.2 0.2 \
-                      --auto-tune-mtl \
-                      --weight-decay=1e-5 \
-                      --update-batchnorm-runningstatistics \
-                      --ema=0.01 \
-                      --train-image-dir {train_image_dir} \
-                      --train-annotations {train_annotations} \
-                      --output={model_output_file}"""
+TRAIN_COMMAND = """cd openpifpaf && \
+                   python -m openpifpaf.train \
+                       --lr=0.05 \
+                       --momentum=0.9 \
+                       --epochs={num_train_epochs} \
+                       --lr-warm-up-epochs=1 \
+                       --lr-decay 220 \
+                       --lr-decay-epochs=30 \
+                       --lr-decay-factor=0.01 \
+                       --batch-size=32 \
+                       --square-edge=385 \
+                       --lambdas 1 1 0.2   1 1 1 0.2 0.2    1 1 1 0.2 0.2 \
+                       --auto-tune-mtl \
+                       --weight-decay=1e-5 \
+                       --update-batchnorm-runningstatistics \
+                       --ema=0.01 \
+                       --train-image-dir {train_image_dir} \
+                       --train-annotations {train_annotations} \
+                       --output={model_output_file}"""
 
-EVAL_VAL_COMMAND = """python -m openpifpaf.eval_coco \
-                  --checkpoint {model_output_file} \
-                  -n 500 \
-                  --long-edge=641 \
-                  --write-predictions \
-                  --dataset val \
-                  --output {eval_output_file}"""
+EVAL_VAL_COMMAND = """cd openpifpaf && \
+                      python -m openpifpaf.eval_coco \
+                          --checkpoint {model_output_file} \
+                          -n 500 \
+                          --long-edge=641 \
+                          --write-predictions \
+                          --dataset val \
+                          --output {eval_output_file}"""
 
-EVAL_OTHER_COMMAND = """python -m openpifpaf.eval_coco \
-                  --checkpoint {model_output_file} \
-                  -n 500 \
-                  --long-edge=641 \
-                  --write-predictions \
-                  --dataset other \
-                  --dataset-image-dir {dataset_image_dir} \
-                  --dataset-annotations {dataset_annotations} \
-                  --output {eval_output_file}"""
+EVAL_OTHER_COMMAND = """cd openpifpaf && \
+                        python -m openpifpaf.eval_coco \
+                            --checkpoint {model_output_file} \
+                            -n 500 \
+                            --long-edge=641 \
+                            --write-predictions \
+                            --dataset other \
+                            --dataset-image-dir {dataset_image_dir} \
+                            --dataset-annotations {dataset_annotations} \
+                            --output {eval_output_file}"""
