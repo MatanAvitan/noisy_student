33e5473c9436727a9aec8089efcaf6a1d9ea72b3
Atalya Alon <atalya.alon@gmail.com>
2020-06-26 02:34:22 +0300

    remove unecessary function

--- src/teacher.py
+++ src/teacher.py
@@ -5,8 +5,3 @@ import os
 class Teacher(Model):
     def __init__(self, model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations):
         super().__init__(model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations)
-
-
-    def create_new_annotations_file(self):
-        # TODO - implement creation of new annotations file in base of teacher predictions (using self._model_eval_file) and images selected using select_new_images
-        pass
