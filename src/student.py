5a938636da10ffc11448811737240386e27a2800
Atalya Alon <atalya.alon@gmail.com>
2020-06-26 16:43:50 +0300

    Add full_data_model to student and teacher classes

--- src/student.py
+++ src/student.py
@@ -3,8 +3,8 @@ import os
 
 
 class Student(Model):
-    def __init__(self, model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations):
-        super().__init__(model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations)
+    def __init__(self, model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations, full_data_model=False):
+        super().__init__(model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations, full_data_model)
 
     def inject_noise(self):
         """
--- src/teacher.py
+++ src/teacher.py
@@ -3,5 +3,5 @@ import os
 
 
 class Teacher(Model):
-    def __init__(self, model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations):
-        super().__init__(model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations)
+    def __init__(self, model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations, full_data_model=False):
+        super().__init__(model_type, model_idx, num_train_epochs, train_image_dir, train_annotations, val_image_dir, val_annotations, next_gen_annotations, full_data_model)
