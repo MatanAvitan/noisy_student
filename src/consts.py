f56d8c6e13fe8b8faa3f290e2e338325b21d7129
Atalya Alon <atalya.alon@gmail.com>
2020-06-28 01:10:07 +0300

    fix CREATE_IMAGES - as bool

--- src/consts.py
+++ src/consts.py
@@ -13,7 +13,7 @@ S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 EXPERIMENT_NAME = os.getenv("EXPERIMENT_NAME")
 AWS_ACCESS_ID = os.getenv("AWS_ACCESS_ID")
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-CREATE_IMAGES = os.getenv("CREATE_IMAGES")
+CREATE_IMAGES = bool(os.getenv("CREATE_IMAGES"))
 
 TRAIN_COMMAND = """cd {openpifpaf_path} && \
                    python -m openpifpaf.train \
