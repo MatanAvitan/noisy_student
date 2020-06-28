9b9c4ed05eea20190dbf42009131b9a4d46d2c18
Atalya Alon <atalya.alon@gmail.com>
2020-06-28 04:03:42 +0300

    Add absolute path in run_noisy_student.sh

--- run_noisy_student.sh
+++ run_noisy_student.sh
@@ -3,23 +3,23 @@
 set -e
 # define environment vars
 
-export ANNOTATIONS_DIR="src/data-mscoco/annotations"
+export ANNOTATIONS_DIR="/home/ubuntu/noisy_student/src/data-mscoco/annotations"
 export NEW_ANNOTATIONS_DIR="new"
 export ORIGINAL_ANNOTATIONS_DIR="original"
 export ORIGINAL_TRAIN_ANNOTATION_FILE="person_keypoints_train2017.json"
 export ORIGINAL_VAL_ANNOTATION_FILE="person_keypoints_val2017.json"
-export OUTPUT_DIR="src/outputs"
-export EVAL_DIR="src/eval"
-export COCOSPLIT_PATH="src/cocosplit.py"
-export OPENPIFPAF_PATH="src/openpifpaf"
-export TRAIN_IMAGE_DIR="src/data-mscoco/images/train2017"
-export VAL_IMAGE_DIR="src/data-mscoco/images/val2017"
-export OPENPIFPAF_PATH="src/openpifpaf"
-export TRAIN_IMAGE_DIR="src/data-mscoco/images/train2017"
-export VAL_IMAGE_DIR="src/data-mscoco/images/val2017"
+export OUTPUT_DIR="/home/ubuntu/noisy_student/src/outputs"
+export EVAL_DIR="/home/ubuntu/noisy_student/src/eval"
+export COCOSPLIT_PATH="/home/ubuntu/noisy_student/src/cocosplit.py"
+export OPENPIFPAF_PATH="/home/ubuntu/noisy_student/src/openpifpaf"
+export TRAIN_IMAGE_DIR="/home/ubuntu/noisy_student/src/data-mscoco/images/train2017"
+export VAL_IMAGE_DIR="/home/ubuntu/noisy_student/src/data-mscoco/images/val2017"
+export OPENPIFPAF_PATH="/home/ubuntu/noisy_student/src/openpifpaf"
+export TRAIN_IMAGE_DIR="/home/ubuntu/noisy_student/src/data-mscoco/images/train2017"
+export VAL_IMAGE_DIR="/home/ubuntu/noisy_student/src/data-mscoco/images/val2017"
 
 # use created virtualenv
 source ~/.venvs/noisy_student_36/bin/activate
 
 # run noisy student
-python src/noisy_student.py
+python /home/ubuntu/noisy_student/src/noisy_student.py
